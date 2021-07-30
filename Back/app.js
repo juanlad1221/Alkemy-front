f2ea3ffe843ebf3f0c0c3b6219b1ccc8e514ad54
Juan Campos <juanlad1221@gmail.com>
2021-07-29 22:58:24 -0300

    modificacion de codigo

--- Back/app.js
+++ Back/app.js
@@ -6,43 +6,8 @@ const sequelize = require("./db/connection")
 const app = express();
 
 app.use(cors())
-app.use(express.urlencoded({ extended:false }))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-/*
-app.post("/api/login", (req , res) => {
-    const user = {
-        id: 1,
-        nombre : "Henry",
-        email: "henry@email.com"
-    }
-
-    jwt.sign({user}, 'secretkey', {expiresIn: '1h'}, (err, token) => {
-        res.json({
-            token
-        });
-    });
-
-});
-
-app.post("/api/getBalance", verifyToken, (req , res) => {
-
-    jwt.verify(req.token, 'secretkey', (error, authData) => {
-        if(error){
-            res.sendStatus(403);
-        }else{
-            res.json({
-                    mensaje: "Post fue creado",
-                    authData
-                });
-        }
-    });
-});*/
-
-
-
-
-
-
 
 
 
@@ -51,10 +16,17 @@ app.use('/api', require('./routes/authentication'))
 
 
 
+
 //--------------------------SERVER---------------------------------------------------------
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => {
-  console.log(`Server corriendo en ${app.get('port')}`)
+    console.log(`Server corriendo en ${app.get('port')}`)
+
+    //db conexion
+    sequelize.authenticate().then(() => {
+        console.log("Database Conected...")
+    }).catch(err => console.log('Error in database...', err))
+
 })
 //------------------------------------------------------------------------------------------
 
@@ -62,14 +34,14 @@ app.listen(app.get('port'), () => {
 
 
 // Authorization: Bearer <token>
-function verifyToken(req, res, next){
-    const bearerHeader =  req.headers['authorization'];
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
 
-    if(typeof bearerHeader !== 'undefined'){
-         const bearerToken = bearerHeader.split(" ")[1];
-         req.token  = bearerToken;
-         next();
-    }else{
+    if (typeof bearerHeader !== 'undefined') {
+        const bearerToken = bearerHeader.split(" ")[1];
+        req.token = bearerToken;
+        next();
+    } else {
         res.sendStatus(403);
     }
 }
