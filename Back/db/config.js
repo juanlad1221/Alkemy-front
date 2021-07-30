6f076d7d9961a6c7045212af447091b87c31d705
Juan Campos <juanlad1221@gmail.com>
2021-07-30 01:00:14 -0300

    se paso el back a otro repo

--- Back/db/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    database: {
-        connectionLimit: 1000,
-        host: 'mysql-39622-0.cloudclusters.net',
-        username: 'admin',
-        password: 'z1Sq4Goy',
-        database: 'db_test'
-    }
-}
